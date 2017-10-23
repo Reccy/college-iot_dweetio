c12ef84bc20c5370508c267fdff9ad606b9745e7
Aaron Meaney <aaronmeaney@hotmail.com>
2017-10-23 15:51:13 +0100

    Added button data collection.

--- dweet_pi.py
+++ dweet_pi.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import grovepi
+import grovepi, time, math
 
 # Returns the current time
 def getCurrentTime():
@@ -9,7 +9,7 @@ def getCurrentTime():
 def getTemperatureSensorData():
 	try:
 		[temperature_raw, humidity_raw] = grovepi.dht(temperature_sensor_port,0)
-		return temperature_raw
+		return math.floor(temperature_raw)
 	except IOError:
 		print "Error reading Temperature and Humidity sensor on port {}".format(temperature_sensor_port)
 
@@ -20,9 +20,12 @@ def getUltrasonicRangerData():
 	except IOError:
 		print "Error reading Ultrasonic Ranger on port {}".format(ultrasonic_ranger_port)
 
-# Returns formatted light sensor data
-def getLightSensorData():
-	return 1;
+# Returns if the button is pressed or not as a boolean
+def getButtonData():
+	try:
+		return grovepi.digitalRead(button_port) == 1
+	except IOError:
+		print "Error reading Light sensor on port {}".format(light_sensor_port)
 
 # Returns formatted RAM usage in MB
 def getMemoryUsageData():
@@ -30,10 +33,11 @@ def getMemoryUsageData():
 
 # Returns formatted data ready for upload as a dictionary
 def getUploadData():
+	time.sleep(0.1)
 	dict = {}
 	dict['temperature'] = getTemperatureSensorData()
 	dict['distance'] = getUltrasonicRangerData()
-	dict['light'] = getLightSensorData()
+	dict['button'] = getButtonData()
 	dict['memory_usage'] = getMemoryUsageData()
 	return dict
 
@@ -44,6 +48,7 @@ def getUploadData():
 # ===================================
 temperature_sensor_port = 6
 ultrasonic_ranger_port = 8
+button_port = 3
 
 # Update loop
 try:
@@ -56,7 +61,7 @@ try:
 		print 'Current Time:      {}'.format(getCurrentTime())
 		print 'Temperature:       {}'.format(upload_data['temperature'])
 		print 'Distance:          {}'.format(upload_data['distance'])
-		print 'Light:             {}'.format(upload_data['light'])
+		print 'Button Pressed:    {}'.format(upload_data['button'])
 		print 'Memory Usage (MB): {}'.format(upload_data['memory_usage'])
 
 		# TODO: Upload the data
