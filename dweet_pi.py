15e4a48405a57c3a24f587dba3b2cd241a19b5a8
Aaron Meaney <aaronmeaney@hotmail.com>
2017-10-24 16:39:22 +0100

    Finished adding data for light, distance, button and cpu usage.

--- dweet_pi.py
+++ dweet_pi.py
@@ -1,52 +1,66 @@
 from datetime import datetime
-import grovepi, time, math
+import grovepi, time, math, sys, random, dweepy, psutil
 
 # Returns the current time
 def getCurrentTime():
 	return datetime.utcnow()
 
-# Returns formatted temperature sensor data
-def getTemperatureSensorData():
+# Returns formatted light sensor data
+def getLightSensorData():#
 	try:
-		[temperature_raw, humidity_raw] = grovepi.dht(temperature_sensor_port,0)
-		return math.floor(temperature_raw)
+		light_raw = grovepi.analogRead(light_sensor_port)
+		time.sleep(0.2)
+		return light_raw
 	except IOError:
-		print "Error reading Temperature and Humidity sensor on port {}".format(temperature_sensor_port)
+		print "Error reading Light sensor on port {}".format(light_sensor_port)
 
 # Returns formatted range sensor data
 def getUltrasonicRangerData():
 	try:
-		return grovepi.ultrasonicRead(ultrasonic_ranger_port)
+		ultrasonic_read_raw = grovepi.ultrasonicRead(ultrasonic_ranger_port)
+		time.sleep(0.2)
+		return ultrasonic_read_raw
 	except IOError:
 		print "Error reading Ultrasonic Ranger on port {}".format(ultrasonic_ranger_port)
 
 # Returns if the button is pressed or not as a boolean
 def getButtonData():
 	try:
-		return grovepi.digitalRead(button_port) == 1
+		button_data_raw = grovepi.digitalRead(button_port) == 1
+		time.sleep(0.1)
+		return button_data_raw
 	except IOError:
-		print "Error reading Light sensor on port {}".format(light_sensor_port)
+		print "Error reading Light sensor on port {}".format(button_port)
 
-# Returns formatted RAM usage in MB
-def getMemoryUsageData():
-	return 1;
+# Returns formatted processor usage in MB
+def getProcessorUsageData():
+	return psutil.cpu_percent()
 
 # Returns formatted data ready for upload as a dictionary
 def getUploadData():
-	time.sleep(0.1)
 	dict = {}
-	dict['temperature'] = getTemperatureSensorData()
+	dict['light'] = getLightSensorData()
 	dict['distance'] = getUltrasonicRangerData()
 	dict['button'] = getButtonData()
-	dict['memory_usage'] = getMemoryUsageData()
+	dict['cpu_usage'] = getProcessorUsageData()
 	return dict
+
+# Uploads a dictionary to the dweet.io service
+def uploadToDweet(dict):
+	try:
+		print "   > dweet.io upload: {}".format(dweepy.dweet_for('aaron_meaney_test', dict))
+	except dweepy.api.DweepyError as e:
+		print "   > dweet.io upload exception: {}".format(e)
+
 
 
 
 # ===================================
 # 'Main' script execution begins here
 # ===================================
-temperature_sensor_port = 6
+
+# Set sensor port numbers
+light_sensor_port = 0
 ultrasonic_ranger_port = 8
 button_port = 3
 
@@ -58,13 +72,14 @@ try:
 
 		# Print upload data
 		print '========================================================='
-		print 'Current Time:      {}'.format(getCurrentTime())
-		print 'Temperature:       {}'.format(upload_data['temperature'])
-		print 'Distance:          {}'.format(upload_data['distance'])
-		print 'Button Pressed:    {}'.format(upload_data['button'])
-		print 'Memory Usage (MB): {}'.format(upload_data['memory_usage'])
+		print 'Current Time:        {}'.format(getCurrentTime())
+		print 'Light:               {}'.format(upload_data['light'])
+		print 'Distance:            {}'.format(upload_data['distance'])
+		print 'Button Pressed:      {}'.format(upload_data['button'])
+		print 'CPU Usage %:         {}'.format(upload_data['cpu_usage'])
 
 		# TODO: Upload the data
+		uploadToDweet(upload_data)
 
 		# TODO: Store it in a SQLite db
 except KeyboardInterrupt:
